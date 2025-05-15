5cb7fec84320dbc6d828be64ab670693c072bdeb
Kai Chi <kay1314233@gmail.com>
2025-05-15 09:44:44 +0800

    feat: first version compeleted.

--- rtsp_reader.py
+++ rtsp_reader.py
@@ -2,10 +2,7 @@ import cv2
 import time
 
 def rtsp_reader_process(shared_images):
-    # rtsp_url = "rtsp://YOUR_CAMERA_ADDRESS"  # ← 替換成你的攝影機串流網址
-    # cap = cv2.VideoCapture(rtsp_url)
-    camera_device = "/dev/video0"
-    cap = cv2.VideoCapture(camera_device, cv2.CAP_V4L2)
+    cap = cv2.VideoCapture(10)
 
     if not cap.isOpened():
         print("❌ 無法開啟 RTSP 串流")
@@ -16,7 +13,7 @@ def rtsp_reader_process(shared_images):
         if not ret:
             print("⚠️ 影像抓取失敗，重試中...")
             time.sleep(1)
-            cap = cv2.VideoCapture(camera_device, cv2.CAP_V4L2)
+            cap = cv2.VideoCapture(10)
             continue
 
         shared_images['original'] = frame
